9800b9614876a5a61286826e77c5a7afedef0af0
RockGo <guozhenqiangg@qq.com>
2026-06-29 18:42:27 +0800

    feat:ssl cert

--- model/gateway.go
+++ model/gateway.go
@@ -1,5 +1,7 @@
 package model
 
+import "time"
+
 type Route struct {
 	Path   string `json:"path" binding:"required"`
 	Target string `json:"target" binding:"required"`
@@ -7,4 +9,20 @@ type Route struct {
 
 type ChangePortRequest struct {
 	Port string `json:"port" binding:"required"`
+}
+
+type SSLConfigRequest struct {
+	Enabled  bool   `json:"enabled"`
+	Port     string `json:"port"`
+	Domain   string `json:"domain"`
+	CertType string `json:"cert_type"`
+}
+
+type SSLConfigResponse struct {
+	Enabled        bool      `json:"enabled"`
+	Port           string    `json:"port"`
+	Domain         string    `json:"domain"`
+	CertType       string    `json:"cert_type"`
+	EffectiveTime  time.Time `json:"effective_time"`
+	ExpirationTime time.Time `json:"expiration_time"`
 }
